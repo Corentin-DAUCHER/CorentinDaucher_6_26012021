c7e53461633907f04ffd610fd9886ddf3f365107
Corentin DAUCHER <corentin.daucher@live.fr>
2021-02-23 11:53:19 +0100

    ...

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,14 +1,38 @@
+//Import du module jsonwebtoken
+
 const jwt = require('jsonwebtoken');
 
+//Export du module
+
 module.exports = (req, res, next) => {
+
+    //Essaie
+
     try{
+
+        //Initialisation du token en séparant la chaine au premier espace
+
         const token = req.headers.authorization.split(' ')[1];
+
+        //Initialisation et vérification du token
+
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+
+        //Initialisation de userId
+
         const userId = decodedToken.userId;
 
+        //Si le userId est présent et est différent du userId présent dans le token
+
         if(req.body.userId && req.body.userId !== userId){
+
+            //Renvoie une erreur
+
             throw('Invalid user ID !');
         }else{
+
+            //Passe à la suite et valide l'authentifiaction
+
             next()
         }
     }
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,8 @@
+//Import du module multer pour la gestion des fichiers entrant
+
 const multer = require('multer');
+
+//Initialisation du MIME_TYPES qui seront pris en charge pour l'upload de fichiers
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -6,15 +10,34 @@ const MIME_TYPES = {
     'image/png': 'png'
 };
 
+//Initialisation de storage
+
 const storage = multer.diskStorage({
+
+    //Initialisation de la destination des fichiers où ils seront stocké
+
     destination: (req, file, callback) => {
         callback(null, 'images');
     },
+
+    //Initialisation du nom de fichier qui sera stocké
+
     filename: (req, file, callback) => {
+
+        //Prend le nom du fchier original et rempalce les espaces par des '_'
+
         const name = file.originalname.split(' ').join('_');
+
+        //Initialisation de l'extension du fichier
+
         const extension = MIME_TYPES[file.mimetype];
+
+        //Renvoie du fichier avec le name + la date actuelle + l'extension
+
         callback(null, name + Date.now() + '.' + extension);
     }
 });
+
+//Export du module
 
 module.exports = multer({storage: storage}).single('image');
